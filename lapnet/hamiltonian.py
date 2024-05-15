e65a3576db27426e119539c0b64b134f19250477
Zekun Shi <derek.zekun.shi@gmail.com>
2024-05-15 13:16:00 +0800

    wip

--- lapnet/hamiltonian.py
+++ lapnet/hamiltonian.py
@@ -12,23 +12,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Evaluating the Hamiltonian on a wavefunction."""
 
+from functools import partial
 from typing import Any, Sequence
 
 import chex
-from lapnet import networks
 import jax
-from jax import lax
 import jax.numpy as jnp
+from jax import lax
+from jax.experimental import jet
 from typing_extensions import Protocol
+
+from lapnet import networks
 
 
 class LocalEnergy(Protocol):
 
-  def __call__(self, params: networks.ParamTree, key: chex.PRNGKey,
-               data: jnp.ndarray) -> jnp.ndarray:
+  def __call__(
+    self, params: networks.ParamTree, key: chex.PRNGKey, data: jnp.ndarray
+  ) -> jnp.ndarray:
     """Returns the local energy of a Hamiltonian at a configuration.
     Args:
       params: network parameters.
@@ -39,13 +42,15 @@ class LocalEnergy(Protocol):
 
 class MakeLocalEnergy(Protocol):
 
-  def __call__(self,
-               f: networks.WaveFuncLike,
-               atoms: jnp.ndarray,
-               charges: jnp.ndarray,
-               nspins: Sequence[int],
-               use_scan: bool = False,
-               **kwargs: Any) -> LocalEnergy:
+  def __call__(
+    self,
+    f: networks.WaveFuncLike,
+    atoms: jnp.ndarray,
+    charges: jnp.ndarray,
+    nspins: Sequence[int],
+    use_scan: bool = False,
+    **kwargs: Any
+  ) -> LocalEnergy:
     """Builds the LocalEnergy function.
 
     Args:
@@ -59,10 +64,49 @@ class MakeLocalEnergy(Protocol):
     """
 
 
+def get_sdgd_idx_set(key, dim, n_sdgd_dim=16):
+  if n_sdgd_dim != 0:
+    idx_set = jax.random.choice(key, dim, shape=(n_sdgd_dim, ), replace=False)
+  else:
+    idx_set = jnp.arange(dim)
+  return idx_set
+
+
+def get_random_vec(
+  key,
+  idx_set,
+  dim,
+  n_sdgd_dim=0,
+  method="sdgd",
+  n_hte_vec=16,
+):
+  """Return a random vector on sdgd sampled dimensions, with Rademacher
+  distribution.
+
+  If with_time, add a one-hot time dimension at the end.
+  """
+  d = n_sdgd_dim or dim
+  if method == "normal":  # HTE
+    rand_vec = jax.random.normal(key, shape=(n_hte_vec, d))
+  elif method == "unit":  # HTE
+    rand_vec = 2 * (
+      jax.random.randint(key, shape=(n_hte_vec, d), minval=0, maxval=2) - 0.5
+    )
+  elif method == "sdgd":
+    rand_vec = jax.vmap(lambda i: jnp.eye(dim)[i])(idx_set) * dim
+  else:
+    raise ValueError
+  n_vec = n_hte_vec
+  d = dim
+  rand_sub_vec = jnp.zeros((n_vec, d)).at[:n_hte_vec, idx_set].set(rand_vec)
+  return rand_sub_vec
+
+
 def local_kinetic_energy(
-    f: networks.LogWaveFuncLike,
-    use_scan: bool = False,
-    forward_laplacian=True) -> networks.LogWaveFuncLike:
+  f: networks.LogWaveFuncLike,
+  use_scan: bool = False,
+  forward_laplacian=True
+) -> networks.LogWaveFuncLike:
   r"""Creates a function to for the local kinetic energy, -1/2 \nabla^2 ln|f|.
 
   Args:
@@ -78,13 +122,29 @@ def local_kinetic_energy(
     -1/2f \nabla^2 f = -1/2 (\nabla^2 log|f| + (\nabla log|f|)^2).
   """
 
-  def _forward_lapl_over_f(params, data):
-      from lapjax import LapTuple, TupType
-      output = f(params, LapTuple(data, is_input=True))
-      return -0.5 * output.get(TupType.LAP) - \
-              0.5 * jnp.sum(output.get(TupType.GRAD)**2)
+  def _randomized_lapl_over_f(params, data, rng):
+    dim = data.shape[0]
+    idx_set = get_sdgd_idx_set(rng, dim)
+    rand_sub_vec = get_random_vec(rng, idx_set, dim)
 
-  def _lapl_over_f(params, data):
+    f_partial = partial(f, params)
+    taylor_2 = lambda i: jet.jet(
+      fun=f_partial,
+      primals=(data, ),
+      series=((jnp.eye(dim)[i], jnp.zeros(dim)), ),
+    )
+    _, (_, hvps) = jax.vmap(taylor_2)(rand_sub_vec)
+    trace_est = jnp.mean(hvps)
+    f_x = jax.grad(f_partial)(data)
+    return -0.5 * (trace_est + jnp.sum(f_x**2))
+
+  def _forward_lapl_over_f(params, data, rng):
+    from lapjax import LapTuple, TupType
+    output = f(params, LapTuple(data, is_input=True))
+    return -0.5 * output.get(TupType.LAP) - \
+            0.5 * jnp.sum(output.get(TupType.GRAD)**2)
+
+  def _lapl_over_f(params, data, rng):
     n = data.shape[0]
     eye = jnp.eye(n)
     grad_f = jax.grad(f, argnums=1)
@@ -93,14 +153,17 @@ def local_kinetic_energy(
 
     if use_scan:
       _, diagonal = lax.scan(
-          lambda i, _: (i + 1, dgrad_f(eye[i])[i]), 0, None, length=n)
+        lambda i, _: (i + 1, dgrad_f(eye[i])[i]), 0, None, length=n
+      )
       result = -0.5 * jnp.sum(diagonal)
     else:
       result = -0.5 * lax.fori_loop(
-          0, n, lambda i, val: val + dgrad_f(eye[i])[i], 0.0)
-    return result - 0.5 * jnp.sum(primal ** 2)
+        0, n, lambda i, val: val + dgrad_f(eye[i])[i], 0.0
+      )
+    return result - 0.5 * jnp.sum(primal**2)
 
   if forward_laplacian:
+    # return _randomized_lapl_over_f
     return _forward_lapl_over_f
   else:
     return _lapl_over_f
@@ -117,8 +180,9 @@ def potential_electron_electron(r_ee: jnp.ndarray) -> jnp.ndarray:
   return jnp.sum(jnp.triu(1 / r_ee[..., 0], k=1))
 
 
-def potential_electron_nuclear(charges: jnp.ndarray,
-                               r_ae: jnp.ndarray) -> jnp.ndarray:
+def potential_electron_nuclear(
+  charges: jnp.ndarray, r_ae: jnp.ndarray
+) -> jnp.ndarray:
   """Returns the electron-nuclearpotential.
 
   Args:
@@ -129,8 +193,9 @@ def potential_electron_nuclear(charges: jnp.ndarray,
   return -jnp.sum(charges / r_ae[..., 0])
 
 
-def potential_nuclear_nuclear(charges: jnp.ndarray,
-                              atoms: jnp.ndarray) -> jnp.ndarray:
+def potential_nuclear_nuclear(
+  charges: jnp.ndarray, atoms: jnp.ndarray
+) -> jnp.ndarray:
   """Returns the electron-nuclearpotential.
 
   Args:
@@ -139,11 +204,13 @@ def potential_nuclear_nuclear(charges: jnp.ndarray,
   """
   r_aa = jnp.linalg.norm(atoms[None, ...] - atoms[:, None], axis=-1)
   return jnp.sum(
-      jnp.triu((charges[None, ...] * charges[..., None]) / r_aa, k=1))
+    jnp.triu((charges[None, ...] * charges[..., None]) / r_aa, k=1)
+  )
 
 
-def potential_energy(r_ae: jnp.ndarray, r_ee: jnp.ndarray, atoms: jnp.ndarray,
-                     charges: jnp.ndarray) -> jnp.ndarray:
+def potential_energy(
+  r_ae: jnp.ndarray, r_ee: jnp.ndarray, atoms: jnp.ndarray, charges: jnp.ndarray
+) -> jnp.ndarray:
   """Returns the potential energy for this electron configuration.
 
   Args:
@@ -155,17 +222,21 @@ def potential_energy(r_ae: jnp.ndarray, r_ee: jnp.ndarray, atoms: jnp.ndarray,
     atoms: Shape (natoms, ndim). Positions of the atoms.
     charges: Shape (natoms). Nuclear charges of the atoms.
   """
-  return (potential_electron_electron(r_ee) +
-          potential_electron_nuclear(charges, r_ae) +
-          potential_nuclear_nuclear(charges, atoms))
+  return (
+    potential_electron_electron(r_ee) +
+    potential_electron_nuclear(charges, r_ae) +
+    potential_nuclear_nuclear(charges, atoms)
+  )
 
 
-def local_energy(f: networks.WaveFuncLike,
-                 atoms: jnp.ndarray,
-                 charges: jnp.ndarray,
-                 nspins: Sequence[int],
-                 use_scan: bool = False,
-                 forward_laplacian=True) -> LocalEnergy:
+def local_energy(
+  f: networks.WaveFuncLike,
+  atoms: jnp.ndarray,
+  charges: jnp.ndarray,
+  nspins: Sequence[int],
+  use_scan: bool = False,
+  forward_laplacian=True
+) -> LocalEnergy:
   """Creates the function to evaluate the local energy.
 
   Args:
@@ -183,11 +254,13 @@ def local_energy(f: networks.WaveFuncLike,
   """
   del nspins
   log_abs_f = lambda *args, **kwargs: f(*args, **kwargs)[1]
-  ke = local_kinetic_energy(log_abs_f, use_scan=use_scan,
-                            forward_laplacian=forward_laplacian)
+  ke = local_kinetic_energy(
+    log_abs_f, use_scan=use_scan, forward_laplacian=forward_laplacian
+  )
 
-  def _e_l(params: networks.ParamTree, key: chex.PRNGKey,
-           data: jnp.ndarray) -> jnp.ndarray:
+  def _e_l(
+    params: networks.ParamTree, key: chex.PRNGKey, data: jnp.ndarray
+  ) -> jnp.ndarray:
     """Returns the total energy.
 
     Args:
@@ -195,10 +268,10 @@ def local_energy(f: networks.WaveFuncLike,
       key: RNG state.
       data: MCMC configuration.
     """
-    del key  # unused
+    # del key  # unused
     _, _, r_ae, r_ee = networks.construct_input_features(data, atoms)
     potential = potential_energy(r_ae, r_ee, atoms, charges)
-    kinetic = ke(params, data)
+    kinetic = ke(params, data, key)
     return potential + kinetic
 
   return _e_l
